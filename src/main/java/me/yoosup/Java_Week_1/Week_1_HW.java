2088a5631ef47d7aa754d8d5d5ded59ec485eab0
Yoosup Shin <brian96shin@gmail.com>
2024-02-28 18:07:21 +0900

    Java 1주차 숙제 완료

---
+++ src/main/java/me/yoosup/Java_Week_1/Week_1_HW.java
@@ -0,0 +1,26 @@
+package me.yoosup.Java_Week_1;
+
+import java.util.Scanner;
+
+public class Week_1_HW {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String title = sc.nextLine();
+        double star = Double.parseDouble(sc.nextLine());
+
+        String[] processes = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            processes[i] = i + 1 + ". " + sc.nextLine();
+        }
+
+        System.out.println(title);
+        System.out.println(star);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(processes[i]);
+        }
+    }
+}
+
